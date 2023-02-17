f9f1733b7b5e11aafbc5b038b61cc1a715effe9d
Thomas Hayner <thom.hayner@gmail.com>
2023-02-17 15:00:46 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DataTreeComponent from './DataTreeComponent';
+import PROVIDED_DATA from './sample-channel-structure.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Inner-Mapped-Data Single Component (PROVIDED_DATA)</h2>
+        <DataTreeComponent data={PROVIDED_DATA.data} />
       </header>
     </div>
   );
---
+++ src/DataTreeComponent.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import styled from 'styled-components';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Collapse from '@material-ui/core/Collapse';
+import { useSpring, animated } from 'react-spring';
+
+//// [START: BULLET POINT SUBCOMPONENTS] ////
+// DEVNOTE: These SvgIcons can be swapped for @material-ui/icons depending on how the overall project is set up
+function MinusSquare(props) {
+  return (
+    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
+    </SvgIcon>
+  );
+}
+
+function PlusSquare(props) {
+  return (
+    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
+    </SvgIcon>
+  );
+}
+
+function CloseSquare(props) {
+  return (
+    <SvgIcon className="close" fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+      <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
+    </SvgIcon>
+  );
+}
+
+function TransitionComponent(props) {
+  const style = useSpring({
+    from: { opacity: 0, transform: 'translate3d(20px,0,0)' },
+    to: { opacity: props.in ? 1 : 0, transform: `translate3d(${props.in ? 0 : 20}px,0,0)` },
+  });
+
+  return (
+    <animated.div style={style}>
+      <Collapse {...props} />
+    </animated.div>
+  );
+}
+
+TransitionComponent.propTypes = {
+  // DEVNOTE: Show the component; triggers the enter or exit states
+  in: PropTypes.bool,
+};
+//// [END: BULLET POINT SUBCOMPONENTS] ////
+
+//// [START: CORE SUBCOMPONENTS] ////
+// DEVNOTE: No styles applied, no functionality applied, this just checks and unchecks
+const StyledCheckbox = styled((props) => <Checkbox {...props} />)``;
+
+const StyledTreeItem = styled((props) => (
+  <TreeItem {...props} TransitionComponent={TransitionComponent} />
+))`
+  & .iconContainer: {
+    & .close: {
+      opacity: 0.3;
+    };
+  };
+  & .group: {
+    margin-left: 7;
+    padding-left: 18;
+    border-left: 1px dashed hsl(100%,100%,100%, 0.4);
+  };
+`;
+
+const StyledTreeView = styled((props) => (
+  <TreeView
+    defaultExpanded={['1']}
+    defaultCollapseIcon={<MinusSquare />}
+    defaultExpandIcon={<PlusSquare />}
+    defaultEndIcon={<CloseSquare />}
+  >
+    {/* DEVNOTE: This is mapped here to comply with the provided data structure. */}
+    {props.data.map(a=> renderTree(a))}
+  </TreeView>
+))`
+  flexGrow: 1;
+`;
+
+StyledTreeView.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      channels: PropTypes.array
+    })
+  )
+};
+//// [END: CORE SUBCOMPONENTS] ////
+
+//// [START: RENDER METHOD] ////
+function renderTree(nodes) {
+
+  return (
+    <StyledTreeItem
+      key={nodes.id}
+      nodeId={nodes.id}
+      label={
+        <div>
+          <FormControlLabel control={<StyledCheckbox />} />
+          {nodes.label}
+        </div>
+      }
+    >
+      {Array.isArray(nodes.channels) ? nodes.channels.map((node) => renderTree(node)) : null}
+    </StyledTreeItem>
+  )
+};
+//// [END: RENDER METHOD] ////
+
+export default function DataTreeComponent({ data }) {
+  return (
+    <StyledTreeView data={data} />
+  )
+};
